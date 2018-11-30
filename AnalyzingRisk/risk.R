2934ee6a6c4fba19c6a33d73483fa53326305845
Heather Goldsby <heather.goldsby@gmail.com>
2018-11-30 12:24:50 -0500

    Update AnalyzingRisk/risk.R
    Added support for Sharpe & Sortino Ratios

--- AnalyzingRisk/risk.R
+++ AnalyzingRisk/risk.R
@@ -8,7 +8,7 @@ library(reshape2)
 ### Get the data
 
 # tickers to get data for
-symbols = c("VFINX","FCNTX", "QQQ", "FB", "AMZN", "NFLX", "GOOG")
+symbols = c("VFINX","FCNTX", "QQQ", "FB", "AMZN", "NFLX", "GOOG", "AAPL")
 
 # start date
 start_date = "2001-01-01"
@@ -45,14 +45,18 @@ x.Returns.yearly <- na.omit(Return.calculate(x.Price.yearly))
 
 # Drawdown - 
 apple_returns <- chart.CumReturns(x.Returns$AAPL, main = "Growth of $1 over time", colorset=rich6equal, legend.loc = "topleft", wealth.index=TRUE, auto.grid=FALSE)
-png(file="apple_returns.png",width=1000,height=500)
-apple_returns
-dev.off()
+# png(file="apple_returns.png",width=1000,height=500)
+# apple_returns
+# dev.off()
 apple_drawdown <- chart.Drawdown(x.Returns$AAPL)
-png(file="apple_drawdown.png",width=1000,height=500)
-apple_drawdown
-dev.off()
-table.Drawdowns(x.Returns[,5,drop=FALSE])
+# png(file="apple_drawdown.png",width=1000,height=500)
+# apple_drawdown
+# dev.off()
+
+all_drawdown <- chart.Drawdown(x.Returns)
+
+# 8 is the index of AAPL
+table.Drawdowns(x.Returns[,8,drop=FALSE])
 
 
 # Pain Index - 
@@ -63,4 +67,23 @@ PainIndex(x.Returns)
 t <- table.SFM(x.Returns[,1:ncol(x.Returns)], x.Returns[,1])
 colnames(t) <- symbols
 t
+
+# Sharpe & Sortino
+# looking at cummulative returns
+cumRet <- chart.CumReturns(x.Returns, main = "", colorset=rich6equal, legend.loc = "topleft", wealth.index=TRUE, auto.grid=FALSE)
+# png(file="cummulative_ret.png",width=1000,height=500)
+# cumRet
+# dev.off()
+
+# set your risk free rate
+Rf = 0
+MAR = 0
+# Rf=.04/12 
+# MAR =.05/12
+t2 <- rbind(Return.cumulative(x.Returns), Return.annualized(x.Returns), SharpeRatio(x.Returns, Rf=Rf), SharpeRatio.annualized(x.Returns, Rf=Rf), SortinoRatio(x.Returns, MAR=MAR), StdDev(x.Returns))
+t2  
+
+
+# More to investigate!
+UlcerIndex(x.Returns)
 
